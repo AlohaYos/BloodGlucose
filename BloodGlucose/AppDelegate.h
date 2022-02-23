920f1e306bb8568d2a00e92ebd34c2594506eb2d
AlohaYos <yos@newtonjapan.com>
2022-02-23 11:10:12 +0900

    Initial Commit

---
+++ BloodGlucose/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BloodGlucose
+//
+//  Created by Yos Hashimoto on 2022/02/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BloodGlucose/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  BloodGlucose
+//
+//  Created by Yos Hashimoto on 2022/02/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
